7df1a0bc1ac8d02f9b80b166336943e2a3f8a5ff
yamr0 <carneymathis778475727@gmail.com>
2026-09-12 22:06:19 +0800

    Disable downloading of luci-app-fancontrol
    Commented out the fancontrol package download.

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# 精简版 + luci-app-fancontrol 风扇面板
-# feeds更新前拉取风扇插件源码
-rm -rf package/luci-app-fancontrol
-git clone https://github.com/fancontrol/luci-app-fancontrol.git package/luci-app-fancontrol
+# 暂时不下载luci-app-fancontrol
+
